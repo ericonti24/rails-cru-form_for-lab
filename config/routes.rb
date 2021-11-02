fd36bb429406a3370ce063b8906b4f41c3bac66c
Eric <ericontiveros123@gmail.com>
2021-11-02 16:31:32 -0700

    commit tree af5c02db4f4bf03157390ec1a660f91e648afd05

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  # get 'songs/show'
+
+  # get 'songs/new'
+
+  # get 'songs/index'
+
+  # get 'songs/edit'
+
+  # get 'genres/show'
+
+  # get 'genres/new'
+
+  # get 'genres/edit'
+
+  # get 'artists/show'
+
+  # get 'artists/new'
+
+  # get 'artists/edit'
+
+  resources :artists
+  resources :genres
+  resources :songs
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
